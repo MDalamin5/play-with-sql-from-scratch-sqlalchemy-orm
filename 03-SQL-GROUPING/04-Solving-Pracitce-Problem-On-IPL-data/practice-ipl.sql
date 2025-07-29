3077c21a8975f26de65092ff03581c2edcb306b0
MDalamin5 <mdal.amin5@northsouth.edu>
2025-07-29 14:36:03 +0600

    practice few quesiton releated to ipl dataset

--- 03-SQL-GROUPING/04-Solving-Pracitce-Problem-On-IPL-data/practice-ipl.sql
+++ 03-SQL-GROUPING/04-Solving-Pracitce-Problem-On-IPL-data/practice-ipl.sql
@@ -1,8 +1,8 @@
 -- find top 5 batsman who make highest run in ipl history.
 
--- SELECT batter, sum(batsman_run) as "runs" FROM cricket.ipl
--- GROUP BY batter
--- ORDER BY  runs DESC LIMIT 5;
+SELECT batter, sum(batsman_run) as "runs" FROM cricket.ipl
+GROUP BY batter
+ORDER BY  runs DESC LIMIT 5;
 
 -- find 2nd highest 6 hitter in ipl history
 
