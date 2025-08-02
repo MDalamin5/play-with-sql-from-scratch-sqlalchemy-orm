28166ec63b5f5bff2677d50e3871c857780372bd
MDalamin5 <mdal.amin5@northsouth.edu>
2025-08-02 12:26:12 +0600

    learn window function Frame

--- 07-SQL-WINDOW-FUNCTiON/05-fisrst-second-nTh-value-WF.sql
+++ 07-SQL-WINDOW-FUNCTiON/05-fisrst-second-nTh-value-WF.sql
@@ -1,16 +1,16 @@
 -- Query related to FIrst, second, nTh value.
 
 -- find this student whose mark is 1 in his branch
--- SELECT *,
--- FIRST_VALUE(name) OVER(PARTITION BY branch ORDER BY marks DESC) as 'Rank-1  Name'
--- FROM students
+SELECT *,
+FIRST_VALUE(name) OVER(PARTITION BY branch ORDER BY marks DESC) as 'Rank-1  Name'
+FROM students
 
 -- find the last value use Frame-window function.
 
--- SELECT *,
--- LAST_VALUE(marks) OVER(PARTITION BY branch ORDER BY marks DESC 
---     ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
--- ) FROM students
+SELECT *,
+LAST_VALUE(marks) OVER(PARTITION BY branch ORDER BY marks DESC 
+    ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+) FROM students
 
 -- find nTh value using window function.
 
