a53593bbe5533bda2b5d12b13e8c54807f4f9fcb
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-18 16:16:31 +0600

    database basic query test as nested sorting

--- 10-MSSQL/select-query.sql
+++ 10-MSSQL/select-query.sql
@@ -1,15 +1,15 @@
 -- Query to select customers with a score of 500 or higher
 
--- SELECT id, country, score
--- FROM Customers WHERE
--- score >=500
+SELECT id, country, score
+FROM Customers WHERE
+score >=500
 
 -- Query to select customers who are from Germany
 
--- SELECT id, country, score
--- FROM Customers WHERE
--- country = 'Germany'
--- order by score DESC;
+SELECT id, country, score
+FROM Customers WHERE
+country = 'Germany'
+order by score DESC;
 
 -- Nested Sorting
 
