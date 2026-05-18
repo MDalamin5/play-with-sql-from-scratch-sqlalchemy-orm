a60bfc6fefb3cb892363ad989b57c33a743825ea
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-18 15:28:58 +0600

    practice very basics sql

---
+++ 10-MSSQL/select-query.sql
@@ -0,0 +1,11 @@
+-- Query to select customers with a score of 500 or higher
+
+-- SELECT id, country, score
+-- FROM Customers WHERE
+-- score >=500
+
+-- Query to select customers who are from Germany
+
+SELECT id, country, score
+FROM Customers WHERE
+country = 'Germany';
