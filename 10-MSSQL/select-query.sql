0ccf7f7ac8d718a46191a1030f47f4a2bde9f193
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-18 16:09:46 +0600

    Nested Sorting in sql table

--- 10-MSSQL/select-query.sql
+++ 10-MSSQL/select-query.sql
@@ -6,6 +6,16 @@
 
 -- Query to select customers who are from Germany
 
-SELECT id, country, score
-FROM Customers WHERE
-country = 'Germany';
+-- SELECT id, country, score
+-- FROM Customers WHERE
+-- country = 'Germany'
+-- order by score DESC;
+
+-- Nested Sorting
+
+SELECT *
+FROM
+customers
+ORDER BY
+country ASC,
+score DESC;
