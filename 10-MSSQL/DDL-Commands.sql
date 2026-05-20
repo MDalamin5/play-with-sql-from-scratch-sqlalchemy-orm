1e4d9d866acc9947aaa02fa407f018ca25d2470f
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-20 12:23:38 +0600

    create a table

---
+++ 10-MSSQL/DDL-Commands.sql
@@ -0,0 +1,13 @@
+-- Create the table with column is id, name, bod, phone
+
+CREATE TABLE persons (
+    id INT NOT NULL,
+    person_name VARCHAR(50) NOT NULL,
+    birth_date DATE,
+    phone VARCHAR(15) NOT NULL,
+    CONSTRAINT pk_persons PRIMARY KEY (id)
+)
+-- view the table.
+
+SELECT *
+FROM persons
