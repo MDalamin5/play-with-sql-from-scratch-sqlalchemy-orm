8b1e2151a2074d3539952983f257bd0832fa7121
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-22 18:22:19 +0600

    build a basic between operator query

--- 10-MSSQL/01-DML/where-all-you-need.sql
+++ 10-MSSQL/01-DML/where-all-you-need.sql
@@ -1,1 +1,11 @@
--- between operators
+-- between operators: if a value in specific range
+-- in between range two number is inclusive
+SELECT *
+FROM customers
+
+
+-- retrieve all customer who score in between 400-900
+SELECT *
+from customers
+WHERE score BETWEEN 400 and 900
+
