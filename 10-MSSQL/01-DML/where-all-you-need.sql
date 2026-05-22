3c7c7f61916ba96def68942d16ba9e547fcd73e7
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-22 18:30:26 +0600

    in operator work as or operator

--- 10-MSSQL/01-DML/where-all-you-need.sql
+++ 10-MSSQL/01-DML/where-all-you-need.sql
@@ -9,3 +9,16 @@ SELECT *
 from customers
 WHERE score BETWEEN 400 and 900
 
+-- membership operator: IN, NOT IN
+-- IN: if a value exits in a list
+-- NOT ID: exactly opposite of IN
+
+-- Retrieve all customer from either Germany or USA
+SELECT *
+FROM customers
+WHERE country = 'USA' or country = 'Germany'
+
+-- more easiest why to write
+SELECT *
+FROM customers
+WHERE country IN ('USA', 'Germany')
