71a495d0a766a8d858010f12413d6120229ba28e
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-24 15:02:17 +0600

    sql anti right join

--- 10-MSSQL/02-JOINs/advanced-join.sql
+++ 10-MSSQL/02-JOINs/advanced-join.sql
@@ -9,3 +9,10 @@ ON C.id = O.customer_id
 WHERE O.order_id is NULl
 
 SELECT * FROM orders
+
+-- Get all orders without matching customers
+SELECT *
+FROM customers as C
+RIGHT JOIN orders as O
+ON C.id = O.customer_id
+WHERE C.id is NULl
