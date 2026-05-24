77bfd2d0bc41fda16506f46cd7e81db806a173b5
mdalamin5 <mdal.amin5@northsouth.edu>
2026-05-24 11:34:48 +0600

    find the customer who did not pass any order yet

---
+++ 10-MSSQL/02-JOINs/advanced-join.sql
@@ -0,0 +1,11 @@
+-- LEFT ANTI JOIN: Return row form left that has NO MATCH in RIght table.
+
+-- Get all customer who haven't placed any order.
+
+SELECT *
+FROM customers as C
+LEFT JOIN orders as O
+ON C.id = O.customer_id
+WHERE O.order_id is NULl
+
+SELECT * FROM orders
