c8b3428ea5dc1d85312b70bac75cda80691ba2c6
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-06 20:27:11 +0600

    multi-table use query define

---
+++ 10-MSSQL/02-JOINs/join-multipule-table-3-plus.sql
@@ -0,0 +1,3 @@
+-- Using Sales retrieve a list of all orders, along with the related customer, products, and employee details, for each order display.
+-- Order is, customer name, Sales, Price, Sales Person Name.
+
