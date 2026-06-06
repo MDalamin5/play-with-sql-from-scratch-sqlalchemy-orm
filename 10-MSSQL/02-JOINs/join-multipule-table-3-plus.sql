77d558f7ea01aebb5262129001a3854a8489dbc4
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-06 20:46:05 +0600

    join 4 table advanced query

--- 10-MSSQL/02-JOINs/join-multipule-table-3-plus.sql
+++ 10-MSSQL/02-JOINs/join-multipule-table-3-plus.sql
@@ -1,3 +1,19 @@
 -- Using Sales retrieve a list of all orders, along with the related customer, products, and employee details, for each order display.
 -- Order is, customer name, Sales, Price, Sales Person Name.
 
+SELECT 
+    odr.OrderID,
+    odr.Sales,
+    cus.FirstName,
+    cus.LastName,
+    prd.Product,
+    empp.FirstName as 'EMployee name'
+    
+FROM Sales.Orders as odr
+LEFT JOIN Sales.Customers as cus
+ON odr.CustomerID = cus.CustomerID
+LEFT JOIN Sales.Products as prd
+ON odr.ProductID = prd.ProductID
+LEFT JOIN Sales.Employees as empp
+ON odr.SalesPersonID = empp.EmployeeID
+
