28f4f939ee240ee1086cfdb596ac85ab9edae51f
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-07 11:49:22 +0600

    except operaions as minus operator

--- 10-MSSQL/03-SETs/set-query.sql
+++ 10-MSSQL/03-SETs/set-query.sql
@@ -14,12 +14,30 @@ FROM Sales.Employees
 
 
 -- Union all its included the Duplicated as well
+-- Union all much faster then the Union
 SELECT
     FirstName,
     LastName
 FROM Sales.Customers
 
 UNION ALL
+
+SELECT
+    FirstName,
+    LastName
+FROM Sales.Employees
+
+
+-- Excepts: Returns all distinct rows from the first query that are not found the second query. [Minus Query]
+
+-- Find employee who are not customer at the same time
+
+SELECT
+    FirstName,
+    LastName
+FROM Sales.Customers
+
+EXCEPT
 
 SELECT
     FirstName,
