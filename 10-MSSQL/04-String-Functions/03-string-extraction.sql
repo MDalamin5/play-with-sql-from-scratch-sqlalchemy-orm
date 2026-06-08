f921af87dcc648d8cedd01182da8789f715ae959
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-08 15:25:18 +0600

    left, righ and substring funcitons

--- 10-MSSQL/04-String-Functions/03-string-extraction.sql
+++ 10-MSSQL/04-String-Functions/03-string-extraction.sql
@@ -4,3 +4,10 @@ SELECT
     LEFT(TRIM(first_name), 2) as 'Name First 2c', 
     RIGHT(TRIM(first_name), 3) as 'Name last 3c'
 FROM customers
+
+-- Retrieve a list of customer first names removing the first character.
+
+SELECT first_name,
+SUBSTRING(TRIM(first_name), 2, LEN(first_name)) as 'Removing the Fist Chat'
+FROM 
+customers
