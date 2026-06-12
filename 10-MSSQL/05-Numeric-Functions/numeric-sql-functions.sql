595d311fbce85aadd07bbf9bf42ce3e68ac8d182
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-12 11:27:25 +0600

    very basics round functions in sql

--- 10-MSSQL/05-Numeric-Functions/numeric-sql-functions.sql
+++ 10-MSSQL/05-Numeric-Functions/numeric-sql-functions.sql
@@ -1,3 +1,8 @@
 -- count the row of the db.
 -- Round UP: 5.126 --> two decimal place: 5.13
 
+SELECT
+3.456,
+ROUND(3.456, 2) as 'Round_2', 
+ROUND(3.456, 1) as 'Round_1',
+ROUND(3.456, 0) as 'Round_0'
