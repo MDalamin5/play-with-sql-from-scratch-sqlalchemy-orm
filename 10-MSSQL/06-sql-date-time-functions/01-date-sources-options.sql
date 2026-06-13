e01ba96f40ae1870076f98ab06c2535b1042d971
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-13 15:13:10 +0600

    day, month, year extraction from a date

--- 10-MSSQL/06-sql-date-time-functions/01-date-sources-options.sql
+++ 10-MSSQL/06-sql-date-time-functions/01-date-sources-options.sql
@@ -4,3 +4,15 @@ CreationTime,
 GETDATE() as 'Today'
 FROM 
 Sales.Orders
+
+-- Extract the prat of the Date
+
+-- Extract Date, month, year
+-- Functions -> Year, Date, Month: Date(2026-06-12) -> 06
+
+SELECT 
+CreationTime, 
+YEAR(CreationTime) Year, 
+MONTH(CreationTime) Month,
+DAY(CreationTime) Day
+FROM Sales.Orders
