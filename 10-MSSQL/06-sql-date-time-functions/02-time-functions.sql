d128c24cb61a5498665019d932809981e993239b
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-16 16:18:57 +0600

    build own custom format date time: Day Wed Jan Q1 2025 12:32:56 PM

--- 10-MSSQL/06-sql-date-time-functions/02-time-functions.sql
+++ 10-MSSQL/06-sql-date-time-functions/02-time-functions.sql
@@ -9,10 +9,17 @@ CreationTime,
 FORMAT(CreationTime, 'dd') as 'Date Number',
 FORMAT(CreationTime, 'ddd') as 'First 3 Latter',
 FORMAT(CreationTime, 'dddd') Full_Name,
-FORMAT(CreationTime, 'mm') Month_as_Number,
-FORMAT(CreationTime, 'mmm') Month_as_2dig,
-FORMAT(CreationTime, 'mmmm') Month_as_full,
+FORMAT(CreationTime, 'MM') Month_as_Number,
+FORMAT(CreationTime, 'MMM') Month_as_2dig,
+FORMAT(CreationTime, 'MMMM') Month_as_full,
 -- won format
 FORMAT(CreationTime, 'dd-MM-yyyy') Own_format
 FROM 
+Sales.Orders
+
+-- P1: Show creationTime using the following format: Day Wed Jan Q1 2025 12:32:56 PM
+SELECT 
+CreationTime,
+'Day ' + FORMAT(CreationTime, 'ddd MMM ') + 'Q' + DATENAME(QUARTER, CreationTime) + FORMAT(CreationTime, ' yyyy') + FORMAT(CreationTime, 'hh:mm:ss tt') as 'Custom Format'
+FROM
 Sales.Orders
