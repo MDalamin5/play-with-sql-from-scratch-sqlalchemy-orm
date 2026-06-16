1bee02d3bb49fb671c5f95579f0f054d179f15c3
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-16 15:59:27 +0600

    different date format

--- 10-MSSQL/06-sql-date-time-functions/02-time-functions.sql
+++ 10-MSSQL/06-sql-date-time-functions/02-time-functions.sql
@@ -3,3 +3,11 @@
 -- dd -> date only
 -- ddd -> First 3 chat of the day: Sun
 -- dddd -> full name of the data : Sunday
+
+SELECT 
+CreationTime,
+FORMAT(CreationTime, 'dd') as 'Date Number',
+FORMAT(CreationTime, 'ddd') as 'First 3 Latter',
+FORMAT(CreationTime, 'dddd') Full_Name
+FROM 
+Sales.Orders
