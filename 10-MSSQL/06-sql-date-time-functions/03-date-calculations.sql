cd72258414cf85270b487a667f2674dc65e056d3
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-17 16:33:26 +0600

    dateadd and problem define

--- 10-MSSQL/06-sql-date-time-functions/03-date-calculations.sql
+++ 10-MSSQL/06-sql-date-time-functions/03-date-calculations.sql
@@ -1,2 +1,9 @@
-SELECT *
-FROM Sales.Orders
+-- DATEADD(): Adds or subtracts a specific time interval to/from a date
+-- add 3 years, reduce 2 month and add 3 day
+-- SYntax: DATEADD(part, interval, date)
+-- DateADD(year, 2, OrderDate)
+
+SELECT 
+*
+FROM
+Sales.Orders
