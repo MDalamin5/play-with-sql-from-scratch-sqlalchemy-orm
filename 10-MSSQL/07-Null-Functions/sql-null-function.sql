9fd14922e69f7b17ee0473f5174862ee52cae0be
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-21 20:33:46 +0600

    coalesce functions replaching

--- 10-MSSQL/07-Null-Functions/sql-null-function.sql
+++ 10-MSSQL/07-Null-Functions/sql-null-function.sql
@@ -6,3 +6,13 @@
 
 -- CHECK NULL: 
     -- ISNULL return true if null else false.
+
+-- ISNULL Functions. ISNULL(value, replacement_value)
+-- ISNULL (Shipping_address, 'unknown')
+
+-- COALESCE(): Returns the first non-null value from the a list.
+
+SELECT 
+*
+FROM 
+Sales.Customers
