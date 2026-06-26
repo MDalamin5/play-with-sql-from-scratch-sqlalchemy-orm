566841d051713bbbe03809feed915a6e30c9f2b4
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-26 11:38:26 +0600

    trim funcitons put singe empty string

--- 10-MSSQL/07-Null-Functions/03-null-vs-empty-str.sql
+++ 10-MSSQL/07-Null-Functions/03-null-vs-empty-str.sql
@@ -12,7 +12,19 @@ WITH Orders AS (
 SELECT
 *, 
 DATALENGTH(Category) CategoryLen
-FROM Orders
+FROM Orders;
 
 -- to remove the empty space and the blank space we use 'TRIM'
 -- TRIM: remove the unwanted leading and trailing spaces from a string.
+
+WITH Orders AS (
+    SELECT 1 Id, 'A' Category UNION
+    SELECT 2, NULL UNION
+    SELECT 3, '' UNION
+    SELECT 4, '   '
+)
+SELECT
+*, 
+DATALENGTH(Category) CategoryLen,
+DATALENGTH(TRIM(Category)) AfterTrim
+FROM Orders
