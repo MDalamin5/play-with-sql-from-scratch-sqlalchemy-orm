9ae43fae5b6d1819078a756644d60874e47a3312
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-26 11:32:35 +0600

    play with the null, emplty str, blank spac

--- 10-MSSQL/07-Null-Functions/03-null-vs-empty-str.sql
+++ 10-MSSQL/07-Null-Functions/03-null-vs-empty-str.sql
@@ -2,3 +2,14 @@
 -- NULL: Don't know anythings about its
 -- Empty String: its a 0 char data
 -- Blank Space: is string value or more space character.
+
+WITH Orders AS (
+    SELECT 1 Id, 'A' Category UNION
+    SELECT 2, NULL UNION
+    SELECT 3, '' UNION
+    SELECT 4, '   '
+)
+SELECT
+*, 
+DATALENGTH(Category) CategoryLen
+FROM Orders
