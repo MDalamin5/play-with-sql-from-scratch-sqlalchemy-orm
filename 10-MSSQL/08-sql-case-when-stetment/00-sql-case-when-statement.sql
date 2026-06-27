6a2cf5b0d69a4262ae15e276014e8f28dd490330
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-27 20:25:13 +0600

    sql syntax as case statement

--- 10-MSSQL/08-sql-case-when-stetment/00-sql-case-when-statement.sql
+++ 10-MSSQL/08-sql-case-when-stetment/00-sql-case-when-statement.sql
@@ -1,2 +1,8 @@
 -- what is Case Statement!
 -- Evaluate a list of conditions and returns a value when the first conditions is met
+
+-- SQL Conditions Syntax
+-- CASE: start of the logics
+--   WHEN conditions1 THEN result1
+--   WHEN conditions2 THEN result2
+-- END: the end of the logics
