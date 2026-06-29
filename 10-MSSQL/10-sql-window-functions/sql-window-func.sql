4fb4d6da75648c5f4baa913e5f3f0a06e4d55af3
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-29 12:01:27 +0600

    solving the windwo functions

---
+++ 10-MSSQL/10-sql-window-functions/sql-window-func.sql
@@ -0,0 +1,6 @@
+-- WINDOW FUNC: Perform calculations (e.g. aggregations) on a specific subset of data, without losing the level of the details of the rows.
+
+-- GROUP BY: simple data-analysis
+-- Window: Advanced data analysis
+
+-- TASK: FIND the total sales across all orders
