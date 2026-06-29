a3a2923b633769469443fb142ec72177d7c6c4c6
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-29 11:49:18 +0600

    sql saggregated functions

--- 10-MSSQL/09-sql-aggregate-functions/sql-aggregate.sql
+++ 10-MSSQL/09-sql-aggregate-functions/sql-aggregate.sql
@@ -1,2 +1,12 @@
 -- They accepted multiply row but output is the singe value
--- e.g. SUM, AVG
+-- e.g. SUM, AVG, COUNT, MAX, MIN
+-- FInd the total number of order, total sales, avg, max, min sales
+
+SELECT 
+COUNT(*) total_number,
+SUM(Sales) total_sales,
+AVG(Sales) avg_sales,
+MAX(Sales) max_sales,
+MIN(Sales) min_sales
+FROM
+Sales.Orders
