f67a7758c090d6980320f0c8e55a58ed8b849cdf
mdalamin5 <mdal.amin5@northsouth.edu>
2026-06-30 12:09:31 +0600

    total number of product order under a product

--- 10-MSSQL/10-sql-window-functions/window-aggrigate-func.sql
+++ 10-MSSQL/10-sql-window-functions/window-aggrigate-func.sql
@@ -1,3 +1,11 @@
 -- SUM, AVT, MIN, MAX, COUNT
 -- why they are called aggregated: its sum all value and return one value.
 -- Syntax: AVG(Sales) OVER (PARTITION by productId Orderby ProductName)
+
+-- TASK: Find the total number of orders for each product.
+
+SELECT 
+COUNT(Sales) OVER(PARTITION BY productId) NumberOfProduct
+FROM
+Sales.Orders
+
